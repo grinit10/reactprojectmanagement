e1b63a90e1e297ac5c58a068883ab5619108ffdb
Arnab Chakraborty <Arnab.Chakraborty@parexelcloud.onmicrosoft.com>
2018-11-18 17:41:20 +0530

    Adding contents to all links

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import DashBoard from './components/dashboard/Dashboard';
 import ProjectDetails from './components/project/ProjectDetails';
+import SignIn from './components/auth/Signin';
+import SignUp from './components/auth/Signup';
+import CreateProject from './components/project/CreateProject';
 
 class App extends Component {
   render() {
@@ -12,6 +15,9 @@ class App extends Component {
           <Navbar></Navbar>
           <Switch>
             <Route path='/project/:id' component={ProjectDetails}></Route>
+            <Route path='/signin' component={SignIn}></Route>
+            <Route path='/signup' component={SignUp}></Route>
+            <Route path='/createproject' component={CreateProject}></Route>
             <Route path='/' component={DashBoard}></Route>
           </Switch>
           {/* <DashBoard></DashBoard> */}
